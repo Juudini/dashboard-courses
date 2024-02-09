9277192b353dc36ed4e71e64d43c7dadf8f3baa7
Juudini <juudinidev@gmail.com>
2024-02-08 22:28:02 -0300

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import ButtonAppBar from "@/components/Navbar";
+
+export default function Home() {
+    return (
+        <>
+            <ButtonAppBar />
+            <main>
+                <h1>Hello World!</h1>
+            </main>
+        </>
+    );
+}
+
