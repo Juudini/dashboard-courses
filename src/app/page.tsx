b86ce22da5611784c5d2a067cf28f19cddd65ebf
Juudini <juudinidev@gmail.com>
2024-02-11 20:09:27 -0300

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Dashboard",
+  description: "Bienvenid@ al dashboard",
+};
+
+export default function Home() {
+  return (
+    <>
+      <h1>Homepage</h1>
+    </>
+  );
+}
