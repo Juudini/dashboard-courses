32142d482b768c126fdf5abbfd9fb65b9da4131c
Juudini <juudinidev@gmail.com>
2024-02-14 20:35:13 -0300

    deleted unnecessary files

--- src/helpers/env.config.ts
+++
@@ -1,7 +0,0 @@
-import "dotenv/config";
-import { get } from "env-var";
-
-export const envs = {
-  MONGODB_URI: get("MONGODB_URI").required().asString(),
-  DB_NAME: get("DB_NAME").required().asString(),
-};
